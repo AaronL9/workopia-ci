f82b4b831b3c2d2bb7461610caae036343430768
Aaron <aaron.lomibao09@gmail.com>
2025-01-05 13:53:57 +0800

    Job Model added

---
+++ app/Models/JobModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class JobModel extends Model
+{
+    protected $table = 'job';
+    protected $returnType = 'object';
+
+    public function getJobs() {
+        return $this->findAll();
+    }
+}
